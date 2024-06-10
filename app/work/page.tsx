3bec6a2130eab881fc10bf13a7876cd30cbdd8af
Ranjeet0302Singh <ranjeet843507@gmail.com>
2024-06-10 23:13:00 +0530

    error done

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
