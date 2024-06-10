f3559a6ef023a291365d10bc16c83e4e7a52e239
Ranjeet0302Singh <ranjeet843507@gmail.com>
2024-06-10 09:56:28 +0530

    resume done

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,5 +1,15 @@
-import React from 'react'
+"use client"
+import {motion} from "framer-motion";
+import {useEffect, useState} from "react";
 
+import {Swiper, SwiperSlide} from "swiper/react"
+import "swiper/css"
+
+import {BsArrowUpRight, BsGithub} from "react-icons/bs"
+import { Tooltip, TooltipContent, TooltipProvider,TooltipTrigger } from "@radix-ui/react-tooltip"; 
+
+import Link from "next/link";
+import Image from "next/image"
 const work = () => {
   return (
     <div>work</div>
